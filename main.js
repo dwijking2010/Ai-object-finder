9135df151a4f741bd8f8ffe1ef60f872892330ef
dwijking2010 <96337755+dwijking2010@users.noreply.github.com>
2022-04-08 12:34:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,69 @@
-status = "";
-
-
-function setup() {
-  canvas = createCanvas(380, 380);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.size(380,380);
-  video.hide();
+cocossdModel = "";
+objectName = "";
+ObjectStatus = "";
+objects = [];
+label = "";
+function setup()
+{
+    canvas = createCanvas(650,370);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(650,370);
+    video.hide();
 }
-
-function modelLoaded() {
-  console.log("Model Loaded!")
-  status = true;
-}
-
 function start()
 {
-  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-  document.getElementById("status").innerHTML = "Status : Detecting Objects";}
- 
-function draw() {
-  image(video, 0, 0, 380, 380);
+    cocossdModel = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    objectName = document.getElementById("object_name_input").value;
+    if(label == objectName)
+    {
+        video.stop();
+        cocossdModel.detect(gotResult);
+        document.getElementById("object_status").innerHTML = "Object mentioned found";
+        synth = window.speechSynthesis;
+        utterThis = new SpeechSynthesisUtterance("object mentioned found");
+        synth.speak(utterThis);
+    }
+    else
+    {
+        document.getElementById("object_status").innerHTML = "Object mentioned not found";
+        synth = window.speechSynthesis;
+        utterThis = new SpeechSynthesisUtterance("object mentioned not found");
+        synth.speak(utterThis);
+    }
+}
+function modelLoaded()
+{
+    console.log("M o d e l  L o a d e d  ! ! ! ! ! !");
+    ObjectStatus = "true";
+    cocossdModel.detect(video,gotResult);
+}
+function draw()
+{
+    image(video,0,0,650,370);
+    if(ObjectStatus == "true")
+    {
+        for(i=0;i<objects.length;i++)
+        {
+            percent = floor(objects[i].length*100);
+            label = objects[i].label;
+            x = objects[i].x;
+            y = objects[i].y;
+            width = objects[i].width;
+            height = objects[i].height;
+        }  
+    }   
+}
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error();
+    }
+    else
+    {  
+        objects = results;
+        console.log(results);
+    }
 }
